894224ad4f152e1dc2e537a221ae548c9571d590
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2022-02-23 15:44:26 +0900

    Update petitutil.js

--- petitutil.js
+++ petitutil.js
@@ -169,4 +169,5 @@ obj=(x,col=[1,1,1,1])=>{
 	console.info('obj',`${Date.now()-t0}ms`);
 	return x;
 },
-hsv=(h=0,s=1,v=1)=>{h*=6;const f=h%1;return[[0,3,1],[2,0,1],[1,0,3],[1,2,0],[3,1,0],[0,1,2]][Math.floor(h)%6].map(x=>v*(1-s*[0,1,f,1-f][x]));};
+hsv=(h=0,s=1,v=1)=>[5,3,1].map(i=>((k=(h*6+i)%6)=>v-Math.max(0,Math.min(1,k,4-k))*s*v)()),
+rgb=(r=0,g=0,b=0)=>((v=Math.max(r,g,b),c=v-Math.min(r,g,b))=>[c&&((v==r?(g-b)/c:v==g?2+(b-r)/c:4+(r-g)/c)/6+1)%1,v&&c/v,v])();
